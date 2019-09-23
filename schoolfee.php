738fbb6b2fa1f946fa18b0ab64aa2b280d829722
gerald <gerald@localhost.localdomain>
2019-09-23 09:52:50 -0700

    %1

--- schoolfee.php
+++ schoolfee.php
@@ -1,8 +1,7 @@
 <?php
 
-
-
 class SchoolFee extends Connect{
+
     public function createSchoolFee(){
         $sql = "CREATE TABLE IF NOT EXISTS schoolfees (
             id INT PRIMARY KEY AUTO_INCREMENT,
@@ -14,6 +13,15 @@ class SchoolFee extends Connect{
     }
 
     public function createFeeTrans(){
-
+        $sql = "CREATE TABLE IF NOT EXISTS feetrans(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            schfeeid INT,
+            amount_paid INT,
+            date_paid date,
+            date_entered date
+            FOREIGN KEY(schfeeid) REFERENCES schoolfees(id)
+        )";
+        $this->link->query($sql);
     }
+
 }
