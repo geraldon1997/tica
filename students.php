0f2411125f3b17ec5ae320fb1f9dab806564d499
gerald <gerald@localhost.localdomain>
2019-09-30 14:25:45 -0700

    %1

--- students.php
+++ students.php
@@ -6,7 +6,7 @@ require 'function/students.php';
 $student = new Student();
 
 if (isset($_POST['addstudent'])) {
-    
+    $student->createStudent();
 }elseif (isset($_POST['updatestudent'])) {
     
 }elseif (isset($_POST['searchstudent'])) {
