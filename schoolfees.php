c127d8ed050879ae9db73d0a8579cc290637bf79
gerald <gerald@localhost.localdomain>
2019-10-01 04:43:58 -0700

    %1

--- schoolfees.php
+++ schoolfees.php
@@ -5,10 +5,26 @@
  $schoolfee = new Schoolfee();
 
  if (isset($_POST['addfeetrans'])) {
-     $schoolfee->createFeeTrans();
- }elseif (isset($_POST['updatefeetrans'])) {
+
+    $schoolfee->createFeeTrans();
+
+}elseif (isset($_POST['updatefeetrans'])) {
      
- }elseif (isset($_POST['searchstudent'])) {
-     $schoolfee->getSchoolfee();
- }
+}elseif (isset($_POST['searchstudent'])) {
+    
+    if (empty($st) && !empty($cl)) {
+        $sql = "SELECT * FROM students WHERE class LIKE '%$cl%' ";
+    }elseif (empty($cl) && !empty($st)) {
+        $sql = "SELECT * FROM students WHERE lname LIKE '%$st%' ";
+    }elseif (!empty($st) && !empty($cl)) {
+        $sql = "SELECT * FROM students WHERE lname LIKE '%$st%' AND class LIKE '%$cl%' ";
+    }
+    $result = $schoolfee->getSchoolfee($sql);
+
+    foreach ($result as $key => $value) {
+        $reg = $value['reg'];
+    }
+    $sql1 = "SELECT * FROM schoolfees WHERE regid = '$reg' ";
+ 
+}
  ?>
